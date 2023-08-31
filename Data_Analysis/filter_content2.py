a05a425b96bb843f2d424b701120d130dd66dc1a
emmanueleduke <eduke.emmanuel@gmail.com>
2023-08-31 10:59:12 +0100

    specific

--- Data_Analysis/filter_content2.py
+++ Data_Analysis/filter_content2.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 df = pd.read_excel(r"C:\Users\Emmanuel\Desktop\output.xlsx",engine="openpyxl")
 
-results = df[df["Grade"].str.match('2')]
+results = df[df["Grade"].str.contains('2')]
 print(results)
